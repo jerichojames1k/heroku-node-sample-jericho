2168abde427ca6d67a537ee7f6ffa894b26b1fba
jerichojames1k <jerichojames1k>
2019-09-24 20:06:59 +0800

    go

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-var app = require('express')();
-var http = require('http').createServer(app);
-var port=process.env.PORT ||3000
-
-app.get('/', function(req, res){
-res.sendFile(__dirname + '/index.ejs');
+app.get('/', function(req, res) {
+    res.render('index.ejs');
 });
 
-http.listen(port, function(){
-console.log('listening on *:' + port);
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+    socket.username = username;
+        io.emit('is_online', socket.username + ' join the chat..');
+    });
 
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', socket.username + ' left the chat..');
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', message );
+    });
+    
+    socket.on('typing', function(username) {
+        io.emit('typing', username);
+    });
+
+});
+
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
 });
