178f93e9c7fd075ffe85796fc8717bc58fcfcc33
jerichojames1k <jerichojames1k>
2019-09-24 20:03:32 +0800

    go

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').createServer(app);
 var port=process.env.PORT ||3000
 
 app.get('/', function(req, res){
-res.sendFile(__dirname + '/index.html');
+res.sendFile(__dirname + '/index.ejs');
 });
 
 http.listen(port, function(){
