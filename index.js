573599dcd70f6a3577bbe7ff6c7cbb03fad8b391
jerichojames1k <jerichojames1k>
2019-09-24 20:50:15 +0800

    clara

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
+
 var app = require('express')();
-var http = require('http').createServer(app);
-var ports=process.env.PORT ||3000
+var http = require('http').Server(app);
+var port = process.env.PORT || 3000
 
-
-http.listen(ports, function(){
-console.log('listening on *:' + ports);
-
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');    
 });
 
+
+
+
+
+
+http.listen(port, function(){                    
+ console.log('listening on *: ' + port);
+});
