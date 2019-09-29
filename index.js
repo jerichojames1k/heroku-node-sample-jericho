e3a1542ba256dd72819aa3c33e907192a428da4b
jerichojames1k <jerichojames1k>
2019-09-29 13:34:09 +0800

    jj

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 var app = require('express')();
 var http = require('http').Server(app);
-var ports = process.env.PORT || 3000
+var port= process.env.PORT || 3000
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');    
 });
 
 
-http.listen(ports, function(){                    
- console.log('listening on *: ' + ports);
+http.listen(port, function(){                    
+ console.log('listening on *: ' + port);
 });
